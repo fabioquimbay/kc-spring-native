6abb97dbbfc5034cbe75b3bcbdde0bf04e257f9d
Fabio Quimbay <fabio.quimbay@gmail.com>
2022-11-18 15:29:39 -0500

    Base implementation

---
+++ src/main/java/com/example/kcspringnative/KcSpringNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kcspringnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcSpringNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcSpringNativeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kcspringnative/KcSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kcspringnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcSpringNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
